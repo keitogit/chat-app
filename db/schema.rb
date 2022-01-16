ff0f32b852f421f4d1e33ffd365f63a05255ff5f
keitogit <kate_m@me.com>
2022-01-16 15:40:00 +0900

    新規作成画面の表示

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_01_15_101505) do
+ActiveRecord::Schema.define(version: 2022_01_15_165922) do
 
   create_table "rooms", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
